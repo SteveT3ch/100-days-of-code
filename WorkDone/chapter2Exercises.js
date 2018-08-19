1f3f29865e966507b8ed39f2626b67e3d8bfd7bd
Derek Kim <ddkim1024@gmail.com>
2018-08-19 00:08:16 -0500

    Added Eloquent JS work

---
+++ WorkDone/chapter2Exercises.js
@@ -0,0 +1,56 @@
+﻿// Eloquent Javascript Exercise Looping a Triangle
+let symbol = "#";
+for (x = 0; x <= 7; x++) {
+    console.log(symbol);
+    (symbol += "#");    
+}
+
+// Eloquent Javascript Exercise FizzBuzz
+for (x = 1; x <= 100; x++) {
+	if ((x % 3 == 0) && (x % 5 == 0)){
+    	console.log("FizzBuzz");
+    } else if (x % 3 == 0){
+        console.log("Fizz");
+    } else if (x % 5 == 0){
+        console.log("Buzz");
+    } else {
+    	console.log(x);
+    }
+}
+
+// Eloquent Javascript Exercise ChessBoard
+let width = 8;
+let height = 8;
+
+
+//Print pattern 1
+const pattern1 = function(x) {  
+    let output = "";
+    for (x = 0; x <= (width-1); x++) {
+	  if (x % 2 == 0) {
+    	  output += " ";
+      } else if (x % 2 == 1){
+    	  output += "#";
+      }
+  } console.log(output);
+};
+
+//Print pattern 2
+const pattern2 = function(x) {
+  output = "";
+  for (x = 0; x <= (width-1); x++) {
+	  if (x % 2 == 0) {
+    	  output += "#";
+      } else if (x % 2 == 1){
+    	  output += " ";
+      }
+  } console.log(output);
+};
+
+for (x = 0; x <= (height-1); x++) {
+    if (x % 2 == 0) {
+        pattern1();
+  } else if (x % 2 == 1) {
+        pattern2();
+  }
+}
---
+++ WorkDone/chapter2Notes.js
@@ -0,0 +1,42 @@
+﻿//let, var, const = binding
+let caught = 5 * 5, ten = 10;
+console.log(ten * ten);
+console.log(ten + caught);
+console.log("the value of x is", caught);
+
+//Math.min() and Math.max()
+console.log(Math.min(caught, ten) + 40);
+
+//prompt() is the annoying web input pop-up on old sites. Modern programming tend to avoid this keyword.
+//Number() converts the string value from input to number. Similar to Python int()
+if (Number.isInteger(ten)) {
+    console.log("The value of this number is", ten);
+}
+// No elif in JavaScript like Java. Must use else if. if statements must also be wrapped in circle brackets unlike Python.
+
+let x = 10;
+if (x < 1) {
+    console.log(x, "is less than 1.")
+} else if (x > 1) {
+    console.log(x, "is greater than 1.")
+} else {
+    console.log(x, "is not a number.")
+}
+
+let num = 0;
+while (num <= 12) {
+    console.log(num);
+    num += 2;
+}
+// do loops runs the body once, then has a while test that must be fulfilled before looping.
+let y = 100
+do {
+    y -= 5
+    console.log(y)
+} while (y > 95)
+// for loops in Javascript requires 3 arguments to run: for([initialization of variable, ie. x = 0]; [condition, ie. x <= 10]; [final-expression: expression to be evaluated at the end of each loop iteration. Updates or increments counter variable, ie. x++])
+for (z = 0; z <= 5; z++) {
+    console.log("z =", z);
+}
+// break; to break out of a loop.
+
